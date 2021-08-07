7ee0e8b2f20d625d0d4a8fd1597295a833c30c91
NeeloBee <boineelolk@gmail.com>
2021-08-08 01:24:22 +0200

    findItemsOver20 failing

---
+++ test/findItemsOver20.tests.js
@@ -0,0 +1,15 @@
+describe('The findItemsOver20' , function(){
+    it('should show you which items in the grocery list have a quantity higher than 20' , function(){
+        assert.deepEqual(results, findItemsOver20(itemList));
+        assert.deepEqual(results2, findItemsOver20(itemList2));
+        assert.deepEqual(results3, findItemsOver20(itemList3));
+    });
+
+    it('should show you which items in the grocery list is' , function(){
+        assert.deepEqual(results, findItemsOver(itemList, 20));
+        assert.deepEqual(results2, findItemsOver(itemList2, 20));
+        assert.deepEqual(results3, findItemsOver(itemList3, 20));
+    });
+
+
+});
