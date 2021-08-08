cd979962ee7c40b8c8c4d4afcc6e9de02556a61b
NeeloBee <boineelolk@gmail.com>
2021-08-08 02:09:14 +0200

    yearsAgo function test passing

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -1,19 +1,9 @@
-/*describe('this test' , function(){
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
-
-        assert.deepEqual([2,2],[2,2]);
+describe('The yearsAgo function test' , function(){
+    it('should return that 1976 is 45 years ago' , function(){
+        assert.equal((new Date().getFullYear() - 1976), yearsAgo(1976));
     });
 
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
-
-        assert.deepEqual([2,2],[2,2]);
-    });
-
-    it('should show you how to use MochaJS' , function(){
-        assert.equal(2,2);
-
-        assert.deepEqual([2,2],[2,2]);
+    it('should show you that 2000 is 21 years ago' , function(){
+        assert.equal((new Date().getFullYear() - 2000), yearsAgo(2000));
     });
 });
