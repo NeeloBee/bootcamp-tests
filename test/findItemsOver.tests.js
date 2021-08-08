dcfc6fb9aa1d8dbb2c82750f27166d437bcf174e
NeeloBee <boineelolk@gmail.com>
2021-08-08 02:25:45 +0200

    findItemsOver failing

---
+++ test/findItemsOver.tests.js
@@ -0,0 +1,14 @@
+describe('The findItemsOver function test' , function(){
+    it('should return items that have a quantity over the threshold' , function(){
+        assert.deepEqual(results, findItemsOver(itemList, 20));
+    });
+
+    it('should return items that have a quantity over the threshold' , function(){
+        assert.deepEqual(results2, findItemsOver(itemList2, 20));
+    });
+
+    it('should return items that have a quantity over the threshold' , function(){
+        assert.deepEqual(results3, findItemsOver(itemList3, 20));
+    });
+
+});
