dfccb67d0ac083f119177d62e9ee1f480ae5f7b8
NeeloBee <boineelolk@gmail.com>
2021-08-09 19:35:16 +0200

    countRegNumber function test passing

---
+++ countRegNumber.js
@@ -0,0 +1,4 @@
+function countRegNumber(regCount) {
+    var countRegNumber = regCount.split(',');
+    return regCount.split(',').length;
+}
--- test/countRegNumber.tests.js
+++ test/countRegNumber.tests.js
@@ -1,11 +1,11 @@
 describe('The countRegNumber function test' , function(){
-    it('should return the number of registration number in the string which is 3' , function(){
-        var regCount =('CA 182736,CY 523519,CJ 812328')
+    it('should return the number of registration number in the string, which is 3' , function(){
+        var regCount = countRegNumber('CA 182736,CY 523519,CJ 812328')
         assert.equal(regCount, 3);
     });
 
     it('should return the number of registration number in the string, which is 1', function(){
-        var regCount = ('CA 182736')
+        var regCount = countRegNumber('CA 182736')
         assert.equal(regCount, 1);
     });
 
